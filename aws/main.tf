38c173504506a31390a2fdfa7a8973aa8d244829
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2023-01-30 23:22:18 +0500

    route table + igw

--- aws/main.tf
+++ aws/main.tf
@@ -22,3 +22,25 @@ resource "aws_subnet" "my-app-subnet-1" {
     Name = "${var.env_prefix}-subnet"
   }
 }
+
+resource "aws_internet_gateway" "my-app-igw" {
+  vpc_id = aws_vpc.my-app-vpc.id
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "my-app-rtb" {
+  vpc_id = aws_vpc.my-app-vpc.id
+
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-app-igw.id
+  }
+
+
+  tags = {
+    Name = "${var.env_prefix}-rbt"
+  }
+}
