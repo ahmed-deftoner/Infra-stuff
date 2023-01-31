d3c42b07cb828e3793b058b3f766c881fc68be6d
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2023-01-31 13:30:45 +0500

    sg vpc

--- aws/main.tf
+++ aws/main.tf
@@ -42,3 +42,34 @@ resource "aws_default_route_table" "main-rtb" {
     Name = "${var.env_prefix}-main-rbt"
   }
 }
+
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.my-app-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+}
+
